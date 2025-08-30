04fc5ee62cc67352eb6e08ae88b5598f2829d131
harshinireddy227-a11y <harshinireddy227@gmail.com>
2025-08-30 12:19:16 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,11 @@
+
+class demo
+{
+    public static void main(string[]args)
+    {
+        int num1=5;
+        int num2=6;
+        int result=num1*num2
+        system.out.println(result);
+    }
+} 
